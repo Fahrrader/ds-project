e938ce74117f8e9ee7bb1c40abab0636e342d04d
Fahrrader <f2r2der@gmail.com>
2019-11-29 20:52:09 +0300

    commit1

---
+++ client.py
@@ -0,0 +1,100 @@
+from os import path
+from time import sleep
+import socket
+
+
+def show_help():
+    print('init         -- to initialize a new repository with this IP address.')
+    print('c [filename] -- create an empty file in your directory.')
+    print('r [filename] -- store and open a file from your directory.')  # TODO if read multiple times, replace local
+    print('w [filename] -- send a file from your computer to the directory with replacing the old one.')
+    print('d [filename] -- delete a file from your directory.')
+    print('i [filename] -- display information about a file in your directory.')
+    print('cp [filename] [path] -- store a copy of a file in the new path.')
+    print('mv [filename] [path] -- store the file in the new path.')
+    print('cd [path]    -- change the current directory.')
+    print('ls           -- list the files in the current directory.')
+    print('md [path]    -- make a new directory in the current directory.')
+    print('d [path]     -- delete a directory from the current directory.')  # TODO support both files and dirs
+    print('help         -- list all commands.')
+    print('exit         -- close the connection and the program.')
+    print('quit         -- same as exit.')
+    print('e            -- same as exit.')
+    print('q            -- same as exit.')
+
+
+def arg_len_error(expected_len):
+    if len(args) != expected_len:
+        print("Unexpected number of arguments.")
+        return True
+
+
+if __name__ == "__main__":
+    SERVER_IP = 'localhost'  # TODO
+    PORT = 8080
+    s = socket.socket()
+    s.connect((SERVER_IP, PORT))  # PORT?
+    print("Connection established.")
+
+    current_dir = "root"  # change to os.path?
+    while True:
+        command = input(current_dir + '>')
+        args = command.split()
+        c = args[0]
+        args = args[1:]
+        if c == 'help':
+            show_help()
+        if c == 'exit' or c == 'quit' or c == 'e' or c == 'q' or c == 'x' or c == 'close':
+            s.shutdown(how=socket.SHUT_RDWR)
+            s.close()
+            print('bye-bye')
+            sleep(0.1)
+            break
+
+        elif c == 'init':
+
+            print("Initialized a new system.")
+        elif c == 'c':
+            if arg_len_error(expected_len=1):
+                continue
+
+        elif c == 'r':
+            if arg_len_error(expected_len=1):
+                continue
+
+        elif c == 'w':
+            if arg_len_error(expected_len=1):
+                continue
+
+        elif c == 'd':
+            if arg_len_error(expected_len=1):
+                continue
+
+        elif c == 'i':
+            if arg_len_error(expected_len=1):
+                continue
+
+        elif c == 'cp':
+            if arg_len_error(expected_len=2):
+                continue
+
+        elif c == 'mv':
+            if arg_len_error(expected_len=2):
+                continue
+
+        elif c == 'cd':
+            if arg_len_error(expected_len=1):
+                continue
+            # TODO set current_dir to new
+            # current_dir = current_dir + '\\' + args[0]  # path.join(current_dir, args[0])
+        elif c == 'ls':
+            if arg_len_error(expected_len=1):
+                continue
+
+        elif c == 'md':
+            if arg_len_error(expected_len=1):
+                continue
+
+        else:
+            print("Unrecognized. Try 'help' if in doubt.")
+
---
+++ name_server.py
@@ -0,0 +1,8 @@
+import socket
+import os
+
+if __name__ == "__main__":
+    #s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    with open('w', 'toto/toto/toto'):
+        print("hey")
+
