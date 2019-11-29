963ef430dcd0b7c419ff7f9a2e730bece8436e3b
Oydi <Oydinosha@mail.ru>
2019-11-30 01:19:32 +0300

    skeleton

--- name_server.py
+++ name_server.py
@@ -1,6 +1,45 @@
 import socket
 import os
 
+users = []
+
+#defs for users
+
+def findUser():
+    pass
+
+def createUser():
+    pass
+
+def findFile():
+    pass
+
+def detectServer():  #для решения оптимального дата центра и размещения файлов
+    pass
+
+def initUser():
+    pass
+
+#defs for servers
+
+checkpoints = []     #придумать во сколько
+
+def isAlive():
+    pass
+
+def hearHeartbeat():
+    pass
+
+def replaceServer():
+    pass
+
+def shareLocation():   #для серваков знать где еще есть файлы
+    pass
+
+def locateNewFile():
+    pass
+
+
 if __name__ == "__main__":
     #s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     with open('w', 'toto/toto/toto'):
